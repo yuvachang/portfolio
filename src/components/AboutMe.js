5a879cb2c5a68b327c7005ec356096079d994ed4
yuvachang <yuva.chang@gmail.com>
2019-04-17 01:43:08 -0400

    started about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function AboutMe() {
   return (
-    <div id='resume'>
-      blahblahblahblahblah
+    <div className='about-me'>
+      <div className='aboutme-title'>
+        designer, software developer, maker
+      </div>
+      <div className='aboutme-blurb'>
+        I am Yuva, blahblah blahblahblahblahblahblahblah blah blah. 
+      </div>
     </div>
   )
 }
