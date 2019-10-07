2c4e936a63edc5594c3b4bd4c781a6868f0ab311
yuvachang <yuva.chang@gmail.com>
2019-10-06 23:13:23 -0400

    updated resume, about page, fixed css background fit

--- src/components/Aboutme/AboutMe.js
+++ src/components/Aboutme/AboutMe.js
@@ -1,78 +1,27 @@
 import React from 'react'
-// import { aboutme, keywords, skills } from '../../data/aboutme.js'
-
-// const pop = text => {
-//   let regex = /[a-zA-Z0-9]/gi
-//   let textArr = text.split(' ')
-//   return textArr
-//     .map((word, idx) => {
-//       let wordOnly = word
-//         .toLowerCase()
-//         .match(regex)
-//         .join('')
-//       if (keywords.includes(wordOnly)) {
-//         // check if phrase is 'software engineer' or 'software engineering'
-//         if (word === 'software' && textArr[idx + 1].includes('engineer')) {
-//           word = word + ' ' + textArr[idx + 1]
-//         }
-//         if (textArr[idx - 1] === 'software' && word.includes('engineer')) return
-//         return (
-//           <span className='pop' key={word + idx}>
-//             <a
-//               className='link'
-//               target='_blank'
-//               href={
-//                 wordOnly === 'mern' || wordOnly === 'pern'
-//                   ? `https://www.google.com/search?q=${wordOnly}+stack`
-//                   : `https://www.google.com/search?q=${wordOnly}`
-//               }>
-//               {word}
-//             </a>
-//           </span>
-//         )
-//       }
-//       if (word === '<br/>') {
-//         return <br />
-//       }
-//       return word
-//     })
-//     .reduce((prev, curr) => [prev, ' ', curr])
-// }
-
-// const highlightEachWord = text => {
-//   return text
-//     .split(', ')
-//     .map((word, idx) => (
-//       <span className='single-pop' key={word + idx}>
-//         <a
-//           className='link'
-//           target='_blank'
-//           href={`https://www.google.com/search?q=${word}`}>
-//           {word}
-//         </a>
-//       </span>
-//     ))
-//     .reduce((prev, curr) => [prev, ', ', curr])
-// }
 
 const AboutMe = () => {
   return (
     <div className='home'>
       <div className='about-header'>Skills</div>
       <div className='about-body'>
-        Javascript, HTML, CSS, React, Redux, Express, Sequelize, Node.js, Git, Firebase, Cloud Firestore, PostgreSQL, SQL, NoSQL, MongoDb, Mongoose, Socket.io, Heroku, Webpack,
-        Rhino-3D, VRay, Adobe {'('}Photoshop, Illustrator, Indesign{')'}, Autocad
+        HTML, CSS, SASS, Javascript, React, Redux, Node.js, Express, Git, Restful API, Firebase,
+        Cloud Firestore, Sequelize, PostgreSQL, NoSQL, Mongoose, MongoDb, Socket.IO, Heroku,
+        Webpack, Expo, React Native, Rhino-3D, VRay, Adobe {'('}Photoshop, Illustrator, Indesign
+        {')'}, Autocad
       </div>
-      <div className='about-header'>Who am I?</div>
+      <div className='about-header'>About</div>
       <div className='about-body'>
-        I am a software engineer with a background in architecture and design. I recently graduated from a coding boot camp {'('}Fullstack Academy
-        {')'} and have skills in Javascript technologies across the stack. Currently seeking work in New York City. 
+        I am a full stack web developer with a background in architecture and design. I graduated from a full stack Javscript coding boot camp {'('}Fullstack Academy
+        {')'} and am actively looking for work in New York City. 
+        <br/>
+        I'm enthusiastic about the process of making and enjoy building things in my free time. 
       </div>
-      <div className='about-header'>What I enjoy</div>
+      {/* <div className='about-header'>What I enjoy</div>
       <div className='about-body'>
         I love making things, digital and physical. I enjoy designing and making furniture and am hoping one day will be able to own a full size CNC
         machine. My other hobbies include building water cooled computers, sewing, photography... 
-      </div>
+      </div> */}
     </div>
   )
 }
