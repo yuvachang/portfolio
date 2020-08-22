183b8ce0b6c9add65f753ae8508f68b302042287
yuvachang <yuva.chang@gmail.com>
2020-08-22 14:57:51 -0400

    Update 8/22/20: new aboutme and resume.

--- src/components/Aboutme/AboutMe.js
+++ src/components/Aboutme/AboutMe.js
@@ -3,32 +3,23 @@ import React from 'react'
 const AboutMe = () => {
   return (
     <div className='home'>
-      {/* <div className='about-header'>About</div> */}
+      <div className='about-header'>About</div>
       <div className='about-body'>
-        Hello!
+        {/* Hello! */}
+        I am Yuva
         <br />
         <br />
-        I am Yuva, a software engineer living in New York City.
-        <br />
-        <br />
-        After my completing my masters at University of Michigan in 2015, I worked in design and
-        architecture. I found myself working extensively with software and doing repetitive tasks
-        that could be automated, and I often used scripts others had written. After a bit of
-        research, I realized the potentional of coding and the excitement of writing your own
-        working code.
-        <br />
-        <br />I decided to take the plunge and took a full-time coding bootcamp called Fullstack
-        Academy. There's constantly new technology and new ways to solve a problem; there's always
-        something new to learn.
+        I'm a software engineer who finds joy and pride in creating dependable solutions for real
+        problems, and I am forever passionate in learning how things work to make our lives easier
+        --be it code, tools and machines, or furniture and buildings.
       </div>
       <div className='about-header'>Skills</div>
       <div className='about-body'>
-        Javascript, Typescript, React, Redux, Node.js, HTML5, CSS3, SASS, Git, Firebase, Cloud
-        Firestore, Express, Restful API, Mongoose, Sequelize, MongoDb, PostgreSQL,
+      <em>Javascript, ES5, ES6, Typescript, React, Redux, Backbone.js, jQuery, Handlebars, Node.js, Gulp, Git, HTML, CSS, SASS,
+        SuiteScript, Firebase, Cloud Firestore, Mocha/Chai, Express, REST, Mongoose, Sequelize, MongoDb, PostgreSQL,
+        Heroku, Github Pages, Webpack, Taggun OCR API
         <br />
-        Expo, React Native, Heroku, Github Pages, Webpack, SPA
-        <br />
-        GitHub Pages, Taggun OCR, Rhino, VRay, Photoshop, Illustrator, Indesign, AutoCAD, SketchUp
+        Other: Rhino, VRay, Photoshop, Illustrator, Indesign, AutoCAD, SketchUp</em>
       </div>
     </div>
   )
